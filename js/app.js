2cda045016071adee6023e9899e1fb97f2450aa8
Stejun <stejun@wp.pl>
2016-10-25 22:26:13 +0200

    test

--- js/app.js
+++ js/app.js
@@ -3,7 +3,25 @@
     
     angular.module('NarrowItDownApp',[])
     
-
+    .controller("NarrowItDownController",["MenuSearchService",function(search){
+        var narrow = this;
+        narrow.input = "";
+        narrow.metoda = function(){
+            search.getMatchedMenuItems(narrow.input);
+        } 
+    }])
+    
+    .service("MenuSearchService",["$http",function($http){
+        this.getMatchedMenuItems = function(searchTerm){
+            $http({
+                method: "GET",
+                url: "davids-restaurant.herokuapp.com/menu_items.json"
+            }).then(function(result){
+                console.log(result);
+            })
+        }
+        
+    }])
     
 
 })()
