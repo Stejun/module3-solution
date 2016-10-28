ad33c45b929f31284cb77bb69e85324b89773a9f
Stejun <stejun@wp.pl>
2016-10-29 00:31:28 +0200

    done

--- js/app.js
+++ js/app.js
@@ -3,25 +3,66 @@
     
     angular.module('NarrowItDownApp',[])
     
-    .controller("NarrowItDownController",["MenuSearchService",function(search){
+    .controller("NarrowItDownController",["MenuSearchService","removeItem",function(search,remove){
         var narrow = this;
         narrow.input = "";
-        narrow.metoda = function(){
-            search.getMatchedMenuItems(narrow.input);
-        } 
+        narrow.found = [];
+        narrow.loader = false;
+        narrow.nothing = false;
+        narrow.check = function(){
+            if(narrow.input){
+                narrow.nothing = false;
+                narrow.loader = true;
+                search.getMatchedMenuItems(narrow.input).then(function(result) {
+                    narrow.found = result.items;
+                    narrow.nothing = result.empty;
+                    narrow.loader = false;
+                });
+            }else{
+                narrow.nothing = true;
+            }
+        };
+        narrow.removeItem = function(index){
+            remove.removeItem(index, narrow.found)
+        };
     }])
-    
     .service("MenuSearchService",["$http",function($http){
         this.getMatchedMenuItems = function(searchTerm){
-            $http({
-                method: "GET",
-                url: "davids-restaurant.herokuapp.com/menu_items.json"
-            }).then(function(result){
-                console.log(result);
-            })
+            return $http.get("http://davids-restaurant.herokuapp.com/menu_items.json")
+                .then(getList)
+                .catch(function(error){
+                    console.log("Something went wrong.");
+            });
+            function getList(result){
+                var foundItems = result.data.menu_items.filter(function(obj){
+                    return obj.description.indexOf(searchTerm) != -1
+                });
+                return {
+                    items:foundItems,
+                    empty: (foundItems.length)?false:true
+                }
+            }
         }
-        
     }])
-    
-
+    .service("removeItem",function(){
+        this.removeItem = function (itemIndex,array) {
+            array.splice(itemIndex, 1);
+        };
+    })
+    .directive("foundItems",function(){
+        return {
+            templateUrl: 'template/itemsTable.template.html',
+            restrict: 'E',
+            scope: {
+                foundItems: "<",
+                onRemove: "&"
+            }
+        }
+    })
+    .directive("loader",function(){
+        return {
+            templateUrl: 'loader/itemsloaderindicator.template.html',
+            restrict: "E"
+        }
+    })
 })()
